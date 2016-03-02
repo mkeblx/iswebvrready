45ad107596cfcc670d208b2467883efd759529ce
Christopher Van <cvan@mozilla.com>
2016-03-02 03:25:35 -0800

    Updates

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 (function () {
-  // Open external links in new tabs.
   document.addEventListener('DOMContentLoaded', function () {
+    // Open external links in new tabs.
     var links = document.querySelectorAll('a[href^="//"], a[href^="http://"], a[href^="https://"]');
     for (var i = 0, len = links.length; i < len; ++i) {
       links[i].setAttribute('target', '_blank');
+    }
+
+    // Strip `.html` from ugly URLs (when served on GitHub Pages).
+    if (location.hostname === 'iswebvrready.org') {
+      var relativeLinks = document.querySelectorAll('a[href$=".html"]:not([href^="//"]):not([href^="http://"]):not([href^="https://"])');
+      for (i = 0, len = relativeLinks.length; i < len; ++i) {
+        relativeLinks[i].setAttribute('href', relativeLinks[i].getAttribute('href').replace(/.html$/, ''));
+      }
     }
   });
 })();
